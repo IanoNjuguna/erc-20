55fabeda40266ae2482f0fb537486bc742a06d69
IanoNjuguna <inc3099@gmail.com>
2025-08-11 01:14:01 +0300

    $uwu v0.1

--- grok420.sol
+++ grok420.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.30;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.9/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
-
